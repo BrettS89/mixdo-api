85e1668b5aed4465c0cb6b850bc74f44e6badfb1
BrettS89 <blsodie@gmail.com>
2018-10-26 18:56:05 -0400

    Not tracking founder ids

--- src/services/mixpanel.js
+++ src/services/mixpanel.js
@@ -1,12 +1,16 @@
 const Mixpanel = require('mixpanel');
 const key = require('../config').mixpanelToken;
 
+const ourIds = ['5bc6b334af3fbc001376ff3e', '5bcd279bc0e7160013a6abf7'];
+
 const mixpanel = Mixpanel.init(key, {
   protocol: 'https'
 });
 
 exports.track = (event, id) => {
-  mixpanel.track(event, {
-    distinct_id: id,
-  });
+  if(ourIds.indexOf(id) === -1) {
+    mixpanel.track(event, {
+      distinct_id: id,
+    });
+  }
 };
